f87409ad26d61fb73096c36fd2b5f8b9ee8e3d69
kr-mustali <mustali.c@knackroot.com>
2024-01-24 18:30:39 +0530

    game update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="App" style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Welcome!</h1>
       <Board />
     </div>
   );
